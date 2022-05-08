64e06a93887d47afd976586ec4d0072e68e26ff2
Royal <sam901009@gmail.com>
2022-05-08 21:55:58 +0800

    無意義

--- nav.php
+++ nav.php
@@ -404,95 +404,7 @@
               </thead>
               
                 <?php 
-                if($_SESSION['identity']=='user'){
-                  echo <<< EOT
-                  <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td><img src="Picture/1.jpg" with="50" heigh="10" alt="Hamburger"></td>
-                    <td>Hamburger</td>
-                  
-                    <td>80 </td>
-                    <td>20 </td>
-                    <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#Hamburger-1">
-                    Edit
-                    </button></td>
-                        <div class="modal fade" id="Hamburger-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">Hamburger Edit</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <div class="modal-body">
-                                <div class="row" >
-                                  <div class="col-xs-6">
-                                    <label for="ex71">price</label>
-                                    <input class="form-control" id="ex71" type="text">
-                                  </div>
-                                  <div class="col-xs-6">
-                                    <label for="ex41">quantity</label>
-                                    <input class="form-control" id="ex41" type="text">
-                                  </div>
-                                </div>
-                      
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
-                              
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                    <td><button type="button" class="btn btn-danger">Delete</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td><img src="Picture/2.jpg" with="10" heigh="10" alt="coffee"></td>
-                    <td>coffee</td>
-                
-                    <td>50 </td>
-                    <td>20</td>
-                    <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#coffee-1">
-                      Edit
-                      </button></td>
-                          <div class="modal fade" id="coffee-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <h5 class="modal-title" id="staticBackdropLabel">coffee Edit</h5>
-                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                  </button>
-                                </div>
-                                <div class="modal-body">
-                                  <div class="row" >
-                                    <div class="col-xs-6">
-                                      <label for="ex72">price</label>
-                                      <input class="form-control" id="ex72" type="text">
-                                    </div>
-                                    <div class="col-xs-6">
-                                      <label for="ex42">quantity</label>
-                                      <input class="form-control" id="ex42" type="text">
-                                    </div>
-                                  </div>
-                        
-                                </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
-                                
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                    <td><button type="button" class="btn btn-danger">Delete</button></td>
-                  </tr> 
-                  </tbody>
-                  EOT;
-                }
-                else{
+                if($_SESSION['identity']=='manager'){
                   $stmt=$conn->prepare("SELECT * from meal where store=:store_name");
                   $stmt->execute(array('store_name' => $store_name));
                   for ($i = 1; $i <= $stmt->rowCount(); $i++) {
@@ -501,7 +413,7 @@
                     <tbody>
                     <tr>
                     <th scope="row">$i</th>
-                    <td><img src="data:$row[5];base64,$row[3]" width="200" heigh="15"></td>
+                    <td><img src="data:$row[5];base64,$row[3]" widt="200" height="100"></td>
                     <td>$row[0]</td>
                     <td>$row[1]</td>
                     <td>$row[2]</td>
