e03862ce206077c4c4f292482010fe049711f02a
Royal <sam901009@gmail.com>
2022-05-09 16:50:56 +0800

    QQQQQ

--- search.php
+++ search.php
@@ -5,12 +5,28 @@
     $dbusername = 'root';
     $dbpassword = '';
     try{
-       echo $_POST['shop_name'].'<br>';
-       echo $_POST['distance'].'<br>';
-       echo $_POST['lowerbound'].'<br>';
-       echo $_POST['upperbound'].'<br>';
-       echo $_POST['meal_name'].'<br>';
-       echo $_POST['catogory'].'<br>';
+        // echo $_POST['shop_name'].'<br>';
+        // echo $_POST['distance'].'<br>';
+        // echo $_POST['lowerbound'].'<br>';
+        // echo $_POST['upperbound'].'<br>';
+        // echo $_POST['meal_name'].'<br>';
+        // echo $_POST['catogory'].'<br>';
+        $shop = $_POST['shop_name'];
+        $dis = $_POST['distance'];
+        $lowerbound = $_POST['lowerbound'];
+        $upperbound = $_POST['upperbound'];
+        $meal = $_POST['meal_name'];
+        $cat = $_POST['catogory'];
+        
+        $shoop = "'' or 1";
+        $conn = new PDO("mysql:host=$dbservername;dbname=$dbname", $dbusername, $dbpassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT * from store where store_name=:store_name");
+        $stmt->execute(array('store_name' => $shop));
+        $row = $stmt ->fetch();
+        if($stmt->rowCount()==0){
+            echo "QQ";
+        }
     }
     catch (Exception $e){
         
