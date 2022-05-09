c9b42eb0a7b54d4a9ee94aa00147c3becd1ccf15
Royal <sam901009@gmail.com>
2022-05-09 19:04:47 +0800

    search

--- search.php
+++ search.php
@@ -17,17 +17,22 @@
         $dis = $_POST['distance'];
         $lowerbound = $_POST['lowerbound'];
         $upperbound = $_POST['upperbound'];
+        if($lowerbound==""){$lowerbound=0;}
+        if($upperbound==""){$upperbound=10000000000;}
+
         $meal = $_POST['meal_name'];
         $cat = $_POST['catogory'];
         $array=array();
-        if($shop==""){
-            $a = "1";
-        }
-        else{
-            $a = "store_name=:store_name";
-            $array['store_name']=$shop;
-        }
-        $stmt = $conn->prepare("SELECT * from store where ".$a);
+        $array['type'] = $cat;
+        $array['lowerbound'] = $lowerbound;
+        $array['upperbound'] = $upperbound;
+        $array['meal_name'] = "%".$meal."%";
+        $array['shop'] = "%".$shop."%";
+        $query = 'select store, dis from(select m,mm,store,type,(latitude*latitude+longitude*longitude) as dis from (select min(price) as m, max(price) as MM, store from meal 
+        where meal_name like :meal_name group by store) as H, store where H.store = store.store_name and type = :type order by dis desc) as qq where qq.m<=:upperbound 
+        and qq.mm>=:lowerbound and store like :shop';
+
+        $stmt = $conn->prepare($query);
         $stmt->execute($array);
         echo $stmt->rowCount();
     }
@@ -36,3 +41,10 @@
         echo $msg;
     }
 ?>
+
+<!-- select m,mm,store,(latitude*latitude+longitude*longitude) as dis from 
+(select min(price) as m, max(price) as MM, store from meal group by store) as H, store where H.store = store.store_name order by dis desc; -->
+
+<!-- select store, dis from(select m,mm,store,type,(latitude*latitude+longitude*longitude) as dis from (select min(price) as m, max(price) as MM, store from meal 
+where meal_name like :meal_name group by store) as H, store where H.store = store.store_name and type = :type order by dis desc) as qq where qq.m<=:upperbound 
+and qq.mm>=:lowerbound and store like :shop; -->
