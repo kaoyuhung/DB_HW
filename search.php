62a8b365c0420525473440ecf17a94ded3ff825a
kaoyuhung <rb910518@gmail.com>
2022-05-09 23:31:54 +0800

    all commited by Liubigbrother

--- search.php
+++ search.php
@@ -43,27 +43,51 @@
                   BETWEEN :lowerbound and :upperbound and meal_name like :meal_name) as H where H.store like :shop and type like :type order by dis desc;';
         $stmt = $conn->prepare($query);
         $stmt->execute($array);
-        $num = $stmt->fetch()[0];
+        $num = (string)$stmt->fetch()[0];
         $num = intval($num);
         if($dis=='far'){
             $query = 'SELECT * from (SELECT DISTINCT(store),type,((longitude-:long)*(longitude-:long)+(latitude-:lat)*(latitude-:lat)) as dis from meal,store where meal.store = store.store_name and price 
             BETWEEN :lowerbound and :upperbound and meal_name like :meal_name) as H where H.store like :shop and type like :type order by dis limit :num;';
-            $array['num'] = floor($num/3);
+            $stmt = $conn->prepare($query);
+            $stmt -> bindValue(":long",(float)$_SESSION['longitude'],PDO::PARAM_STR);
+            $stmt -> bindValue(":lat",(float)$_SESSION['latitude'],PDO::PARAM_STR);
+            $stmt -> bindValue(":lowerbound",intval($lowerbound),PDO::PARAM_INT);
+            $stmt -> bindValue(":upperbound",intval($upperbound),PDO::PARAM_INT);
+            $stmt -> bindValue(":meal_name","%".$meal."%",PDO::PARAM_STR);
+            $stmt -> bindValue(":shop","%".$shop."%",PDO::PARAM_STR);
+            $stmt -> bindValue(":type",$cat,PDO::PARAM_STR);
+            $stmt -> bindValue(":num",(int)floor($num/3),PDO::PARAM_INT);
         }
         if($dis=='medium'){
             $query = 'SELECT * from (SELECT DISTINCT(store),type,((longitude-:long)*(longitude-:long)+(latitude-:lat)*(latitude-:lat)) as dis from meal,store where meal.store = store.store_name and price 
             BETWEEN :lowerbound and :upperbound and meal_name like :meal_name) as H where H.store like :shop and type like :type order by dis limit :num1 offset :num;';
-            $array['num'] = floor($num/3);
-            $array['num1'] = floor($num*2/3)-floor($num/3);
+            $stmt = $conn->prepare($query);
+            $stmt -> bindValue(":long",(float)$_SESSION['longitude'],PDO::PARAM_STR);
+            $stmt -> bindValue(":lat",(float)$_SESSION['latitude'],PDO::PARAM_STR);
+            $stmt -> bindValue(":lowerbound",intval($lowerbound),PDO::PARAM_INT);
+            $stmt -> bindValue(":upperbound",intval($upperbound),PDO::PARAM_INT);
+            $stmt -> bindValue(":meal_name","%".$meal."%",PDO::PARAM_STR);
+            $stmt -> bindValue(":shop","%".$shop."%",PDO::PARAM_STR);
+            $stmt -> bindValue(":type",$cat,PDO::PARAM_STR);
+            $stmt -> bindValue(":num",(int)(floor($num/3)),PDO::PARAM_INT);
+            $stmt -> bindValue(":num1",(int)(floor($num*2/3)-floor($num/3)),PDO::PARAM_INT);
         }
         if($dis=='near'){
             $query = 'SELECT * from (SELECT DISTINCT(store),type,((longitude-:long)*(longitude-:long)+(latitude-:lat)*(latitude-:lat)) as dis from meal,store where meal.store = store.store_name and price 
             BETWEEN :lowerbound and :upperbound and meal_name like :meal_name) as H where H.store like :shop and type like :type order by dis limit :num2 offset :num1';
-            $array['num1'] = floor($num*2/3);
-            $array['num2'] = $num-floor($num*2/3);
+            $stmt = $conn->prepare($query);
+            $stmt -> bindValue(":long",(float)$_SESSION['longitude'],PDO::PARAM_STR);
+            $stmt -> bindValue(":lat",(float)$_SESSION['latitude'],PDO::PARAM_STR);
+            $stmt -> bindValue(":lowerbound",intval($lowerbound),PDO::PARAM_INT);
+            $stmt -> bindValue(":upperbound",intval($upperbound),PDO::PARAM_INT);
+            $stmt -> bindValue(":meal_name","%".$meal."%",PDO::PARAM_STR);
+            $stmt -> bindValue(":shop","%".$shop."%",PDO::PARAM_STR);
+            $stmt -> bindValue(":type",$cat,PDO::PARAM_STR);
+            $stmt -> bindValue(":num1",(int)floor($num*2/3),PDO::PARAM_INT);
+            $stmt -> bindValue(":num2",(int)($num-floor($num*2/3)),PDO::PARAM_INT);
         }
-        $stmt = $conn->prepare($query);
-        $stmt->execute($array); 
+        
+        $stmt->execute(); 
         $_SESSION['search'] = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC)); 
         $_SESSION['dist'] = $dis;
     }
